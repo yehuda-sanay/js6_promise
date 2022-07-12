5950ac9b995d7555df6b0ed681a88e0ce243a6c7
yehuda-sanay <yehudasanay1@gmail.com>
2022-07-12 17:12:39 +0300

    12.07.22

--- main.js
+++ main.js
@@ -1,44 +1,65 @@
-function getPromise(){
-    return new Promise((resolve, reject)=>{
-    const clock=new Date().getHours;
-        if(clock<11){
-            resolve("good morning")
-        }
-        reject("good afternun")
-    });
-}
-getPromise()
-.then((data)=>{document.body.innerHTML+=data;})
-.catch((err)=>{document.body.innerHTML+=err;})
-
-function ifNUmberDivisibleNumber(num1,num2){
-    return new Promise((resolve, reject)=>{
-        if(num2%num1==0){
-            resolve("yes")
-        }
-        reject("no")
-    });
-}
-function forButten(){
-ifNUmberDivisibleNumber(3,6)
-.then((data)=>{div_id.innerHTML=`<h1>${data}</h1>`;})
-.catch((err)=>{div_id.innerHTML=`<h1>${err}</h1>`;})
-}
-
-// function ifNumberInArray(num, myArray) {
-//   return new Promise((resolve, reject) => {
-//     for (let i = 0; i < myArray.Length; i++) {
-//       if (num == myArray[i]) {
-//         resolve("yes");
-//       }
-//     }
-//     reject("no");
-//   });
+//שאלה 1
+// function getPromise(){
+//     return new Promise((resolve, reject)=>{
+//     const clock=new Date().getHours();
+//         if(clock<11){
+//             resolve("good morning");
+//         }
+//         reject("good afternun");
+//     });
 // }
-// ifNumberInArray(3, [30, 40, 50, 3, 65, 95])
-//   .then((data) => {
-//     div_id.innerHTML = `<h1>${data}</h1>`;
-//   })
-//   .catch((err) => {
-//     div_id.innerHTML = `<h1>${err}</h1>`;
-//   });
+// //שאלה 2
+// getPromise()
+// .then((data)=>{document.body.innerHTML+=data;})
+// .catch((err)=>{document.body.innerHTML+=err;})
+
+//שאלה 3
+// function ifNUmberDivisibleNumber(num1,num2){
+//     return new Promise((resolve, reject)=>{
+//         if(num2%num1==0){
+//             resolve("yes")
+//         }
+//         reject("no")
+//     });
+// }
+
+// //שאלה 4
+// function forButten(){
+// ifNUmberDivisibleNumber(3,6)
+// .then((data)=>{div_id.innerHTML=`<h1>${data}</h1>`;})
+// .catch((err)=>{div_id.innerHTML=`<h1>${err}</h1>`;})
+// }
+
+
+//שאלה 5
+
+// function printNumberInArray(num,myArray){
+//     return new Promise((resolve,reject) => {
+//         myArray.forEach(element => {
+//             if(element==num){
+//                 resolve("yes")
+//             }
+//         });
+//         reject("no")
+        
+//     })
+// }
+// //שאלה 6
+// printNumberInArray(5,[7,4,8,5])
+// .then((data)=>{div_id.innerHTML+=`<h1>${data}</h1>`})
+// .catch((nodata)=>{div_id.innerHTML+=`<h1>${nodata}</h1>`})
+
+//שאלה 8
+// async function callSella(){
+//     try{
+//       return await printNumberInArray(5,[7,4,8,5])
+//       .then((data)=>{div_id.innerHTML+=`<h1>${data}</h1>`})
+
+
+//     }
+//     catch(err){
+//         console.log("errr");
+
+//     }
+// }
+// callSella()
