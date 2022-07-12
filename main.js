2bdee99ae387e9fc0220c1984b7d20a2c31ec02d
yehuda-sanay <yehudasanay1@gmail.com>
2022-07-12 22:34:10 +0300

    12.07.22

--- main.js
+++ main.js
@@ -73,19 +73,70 @@
 //שאלה 1
 
 
-const API="https://moviesmern.herokuapp.com/movies/all"
-async function sonefunc(){
-    document.getElementById("button_id").disabled = true
-    try{
-        await fetch(`${API}`).then((Response)=>{console.log(Response)})
-    }
-    catch(err){
+// const API="https://moviesmern.herokuapp.com/movies/all"
+// async function sonefunc(){
+//     document.getElementById("button_id").disabled = true
+//     div_id.innerHTML=`<img style="height: 10vh;" src="https://thumbs.gfycat.com/GrimyEthicalAsianpiedstarling-size_restricted.gif" alt="">`
+//     try{
+//         await fetch(`${API}`).then((Response)=>{console.log(Response)})
+//     }
+//     catch(err){
 
-    }
-    finally{
-        document.getElementById("button_id").disabled = false 
-    }
-}
+//     }
+//     finally{
+//         document.getElementById("button_id").disabled = false 
+//         div_id.innerHTML=""
+//     }
+// }
+
+//שאלה 5
+
+// const API2="https://cat-fact.herokuapp.com/facts"
+// async function somefunc(){
+//     document.getElementById("button2").disabled=true
+//     div_id.innerHTML=`<img style="height: 10vh;" src="https://media4.giphy.com/media/26n6WywJyh39n1pBu/giphy.gif">`
+    
+//     try{
+//         await fetch(`${API2}`).then((Response)=>{console.log(Response)})
+
+//     }
+//     catch(err){
+
+//     }
+//     finally{
+//         document.getElementById("button2").disabled=false
+//         div_id=""
+//     }
+// }
+
+// const API3="https://placekeanu.com/200/150"
+// async function somefunc3(){
+//     try{
+//     await fetch(`${API3}`).then((Response)=>{div_id.innerHTML=`<img src=${Response}>`})
+//     }
+//     catch(err){alert(err)
+
+//     }
+//     finally{
+
+//     }
+// }
+// somefunc3()
+
+// const API="https://api.jikan.moe/v4/anime"
+// async function somfunc(){
+//     try{
+//         await fetch(`${API}`).then((Response)=>{console.log(Response)})
+//     }
+//     catch(err){
+
+//     }
+//     finally{
+
+//     }
+// }
+// somfunc()
+
 
 
 
